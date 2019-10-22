d76aea82e473781482e76e5c108c3b18a4f83311
calt08 <cesarlopez2808@gmail.com>
2019-10-22 02:12:20 -0400

    Almost killed my pc in an infinite loop, worked in my loop, found some good code on the internet that helped me a lot

--- clicker.py
+++ clicker.py
@@ -1,6 +1,65 @@
-from pynput.keyboard import Key, Controller
-import time
+from pynput.keyboard import Key, Controller, Listener
+import time, threading
 
-keyboard = Controller()
-time.sleep(2)
+def on_press_start(key):
+    if key == Key.f12:
+        print('starting...')
+        return False
+
+def on_press_loop(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press_start) as listener:
+    listener.join() # wait for F11...
+
+with Listener(on_press=on_press_loop) as listener:
+    BOARD = Controller()
+    time.sleep(5)
+    for _ in range(50):
+        print('still running...')
+        BOARD.press(Key.space)
+        BOARD.release(Key.space)
+        time.sleep(0.25)
+        if not listener.running:
+            break
+
+# time.sleep(2)
+# BOARD = Controller()
+
+# # for i in range(10):
+# #     BOARD.press(Key.space)
+# #     BOARD.release(Key.space)
+# #     time.sleep(0.25)
+# try:
+#     for i in range(20):
+#         BOARD.press(Key.space)
+#         BOARD.release(Key.space)
+#         time.sleep(0.25)
+# except KeyboardInterrupt:
+#     pass
+
+# printOnConsole, on_release=c
+
+# t1 = threading.Thread(target=)
+
+# with Listener(on_press=clickButton) as l:
+#     try:
+#         l.join()
+#     except Exception as e:
+#         print('{0} was pressed'.format(e.args[0]))
+
+# def printOnConsole(key):
+#     BOARD = Controller()
+#     if key == keyboard.Key.esc:
+#         BOARD.press(Key.space)
+#         BOARD.release(Key.space)
+
+# def clickButton(key):
+#     BOARD = Controller()
+#     if key == keyboard.Key.esc:
+#         x += 1
+#         while(x == True):
+#             BOARD.press(Key.space)
+#             BOARD.release(Key.space)
 
