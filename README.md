7f0351723896a8a58637ad8641bb9900ef67a7bc
calt08 <cesarlopez2808@gmail.com>
2019-10-23 01:06:10 -0400

    Some improvements

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-JUST A PROGRAM TO AUTOCLICK SPACEBAR
+#JUST A PROGRAM TO AUTOCLICK SPACEBAR
 
 I created it to help me with the selection of my teachers on school
 
--- clicker.py
+++ clicker.py
@@ -1,8 +1,8 @@
 from pynput.keyboard import Key, Controller, Listener
-import time, threading
+import time
 
 def on_press_start(key):
-    if key == Key.f12:
+    if key == Key.space:
         print('starting...')
         return False
 
@@ -11,55 +11,16 @@ def on_press_loop(key):
         return False
 
 with Listener(on_press=on_press_start) as listener:
-    listener.join() # wait for F11...
+    listener.join() # Waits for the spacebar to be pressed
 
 with Listener(on_press=on_press_loop) as listener:
     BOARD = Controller()
-    time.sleep(5)
+    time.sleep(2)
     while True:
         print('still running...')
         BOARD.press(Key.space)
         BOARD.release(Key.space)
-        time.sleep(0.25)
+        time.sleep(0.01)
         if not listener.running:
             break
-
-# time.sleep(2)
-# BOARD = Controller()
-
-# # for i in range(10):
-# #     BOARD.press(Key.space)
-# #     BOARD.release(Key.space)
-# #     time.sleep(0.25)
-# try:
-#     for i in range(20):
-#         BOARD.press(Key.space)
-#         BOARD.release(Key.space)
-#         time.sleep(0.25)
-# except KeyboardInterrupt:
-#     pass
-
-# printOnConsole, on_release=c
-
-# t1 = threading.Thread(target=)
-
-# with Listener(on_press=clickButton) as l:
-#     try:
-#         l.join()
-#     except Exception as e:
-#         print('{0} was pressed'.format(e.args[0]))
-
-# def printOnConsole(key):
-#     BOARD = Controller()
-#     if key == keyboard.Key.esc:
-#         BOARD.press(Key.space)
-#         BOARD.release(Key.space)
-
-# def clickButton(key):
-#     BOARD = Controller()
-#     if key == keyboard.Key.esc:
-#         x += 1
-#         while(x == True):
-#             BOARD.press(Key.space)
-#             BOARD.release(Key.space)
 
